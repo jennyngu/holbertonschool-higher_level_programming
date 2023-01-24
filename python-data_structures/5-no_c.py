eb728485791e2bdcf3653273ef21112401094c50
Jenny <5704@holbertonstudents.com>
2023-01-23 16:22:06 -0800

    Changed to create new string with chars that are not c or C. Moved return statement out of for loop

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    new_string = ""
+    letter_c = 'cC'
     for letter in range(len(my_string)):
-        if my_string[letter] == 'c' or my_string[letter] == 'C':
-            return my_string[:letter] + my_string[letter + 1:]
+        if my_string[letter] not in letter_c:
+            new_string = new_string + my_string[letter]
+    return new_string
