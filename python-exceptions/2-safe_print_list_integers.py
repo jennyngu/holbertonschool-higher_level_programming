212aa6d38af1ce2d8eb7bca4ae54f8f02abf3f2f
Jenny <5704@holbertonstudents.com>
2023-01-27 04:31:12 -0800

    Added a func that prints first x elements

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    list_len = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end='')
+            list_len = list_len + 1
+        except(ValueError, TypeError):
+            continue
+    print()
+    return list_len
