b0ecc9388ab769d5e9bfa01a6dbec9db9687fd95
Jenny <5704@holbertonstudents.com>
2023-01-27 22:56:12 -0800

    Changed except statement to print error based on type

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -5,9 +5,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except TypeError:
-        sys.stderr.write("Exception: Unknown format code 'd' for object of type 'str'\n")
-        return False
-    except ValueError:
-        sys.stderr.write("Exception: Unknown format code 'd' for object of type 'str'\n")
+    except Exception as e:
+        print("Exception: {}".format(e))
         return False
