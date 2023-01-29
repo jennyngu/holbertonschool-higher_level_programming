d58d3be3dfcd2320ee4a2af2c8e5e5aaf0a1111d
Jenny <5704@holbertonstudents.com>
2023-01-29 03:19:14 -0800

    Added if statement for case of None and not string type. Fixed pycodestyle

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if not roman_string and type(roman_string) != str:
+        return 0
     roman_dict = {'I': 1,
                   'V': 5,
                   'X': 10,
@@ -10,7 +12,7 @@ def roman_to_int(roman_string):
     roman_num = 0
     for value in range(len(roman_string)):
         if value + 1 < len(roman_string) and roman_dict[roman_string[value]] \
-        < roman_dict[roman_string[value + 1]]:
+           < roman_dict[roman_string[value + 1]]:
             roman_num = roman_num - roman_dict[roman_string[value]]
         else:
             roman_num = roman_num + roman_dict[roman_string[value]]
