ec64b72893186a8a1421be07c9d31bba4cabc9c5
Jenny <5704@holbertonstudents.com>
2023-02-05 01:01:09 -0800

    Added a public class attribute that counts the instances of rectangles

---
+++ python-more_classes/6-rectangle.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python3
+"""Defines a class named Rectangle"""
+
+
+class Rectangle:
+    """
+    A class that defines a rectangle
+    """
+    number_of_instances = 0
+
+    def __init__(self, width=0, height=0):
+        """
+        Initialises the rectangle with the given dimensions
+        """
+        self.width = width
+        self.height = height
+        type(self).number_of_instances = type(self).number_of_instances + 1
+
+    @property
+    def width(self):
+        """
+        Property getter to retrieve width size
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        Property setter to set width size
+        """
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        Property getter to retrieve height
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        Property setter to set the height
+        """
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """
+        Defines the area of the rectangle
+        """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """
+        Defines the perimeter of the rectangle
+        """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """
+        Prints the rectange with the '#' symbol
+        """
+        if self.__width == 0 or self.__height == 0:
+            return f""
+        else:
+            string = ""
+            for i in range(1, self.__height):
+                string = string + '#' * self.__width + '\n'
+            else:
+                string = string + '#' * self.__width
+            return string
+
+    def __repr__(self):
+        """
+        Represents the repr() func to return
+        a printable representation of the given object
+        """
+        return "Rectangle({}, {})".format(self.__width, self.__height)
+
+    def __del__(self):
+        """
+        Deleting an instance
+        """
+        type(self).number_of_instances = type(self).number_of_instances - 1
+        print("Bye rectangle...")
