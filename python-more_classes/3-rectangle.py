778688866e9d309629efce173964d1833aa755ef
Jenny <5704@holbertonstudents.com>
2023-02-05 00:52:16 -0800

    Added the str method that prints the rectangle

---
+++ python-more_classes/3-rectangle.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+"""Defines a class named Rectangle"""
+
+
+class Rectangle:
+    """
+    A class that defines a rectangle
+    """
+    def __init__(self, width=0, height=0):
+        """
+        Initialises the rectangle with the given dimensions
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """
+        Property getter to retrieve width size
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        Property setter to set width size
+        """
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        Property getter to retrieve height
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        Property setter to set the height
+        """
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """
+        Defines the area of the rectangle
+        """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """
+        Defines the perimeter of the rectangle
+        """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """
+        Prints the rectange with the '#' symbol
+        """
+        if self.__width == 0 or self.__height == 0:
+            return f""
+        else:
+            string = ""
+            for i in range(1, self.__height):
+                string = string + '#' * self.__width + '\n'
+            else:
+                string = string + '#' * self.__width
+            return string
