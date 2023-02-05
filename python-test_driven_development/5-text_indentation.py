0a83f21e5c39b9df34aa3669cf155dce4bc76c39
Jenny <5704@holbertonstudents.com>
2023-02-05 03:20:22 -0800

    Added documentation

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+"""
+Adds 2 new lines to a piece of text
+"""
+
+
 def text_indentation(text):
+    """
+    Adds 2 new lines after encounter with ., ?, and : characters
+
+    text: text to alter
+    """
     if type(text) != str:
         raise TypeError("text must be a string")
     else:
