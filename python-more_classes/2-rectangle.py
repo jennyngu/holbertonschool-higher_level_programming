bd92e7e90ed75b787ef1b3564e13558fdb00d088
Jenny <5704@holbertonstudents.com>
2023-02-05 00:46:40 -0800

    Attempt to fix module documentation

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -61,4 +61,5 @@ class Rectangle:
         """
         if self.__width == 0 or self.__height == 0:
             return = 0
-        return 2 * (self.__width + self.__height)
+        else:
+            return 2 * (self.__width + self.__height)
