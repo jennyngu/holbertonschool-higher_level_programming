e71100a0e95a9963030e994aff32cd8a829712a9
Jenny <5704@holbertonstudents.com>
2023-02-05 02:54:36 -0800

    Added a function that prints a square with #

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def print_square(size):
+    if type(size) != int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if type(size) == float and size < 0:
+        raise TypeError("size must be an integer")
+    if size == 0:
+        print()
+    for i in range(1, size + 1):
+        for j in range(1, size + 1):
+            if j <= i:
+                print('#', end='')
+            else:
+                print('#', end= '')
+        print()
