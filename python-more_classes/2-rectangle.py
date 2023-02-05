a471368a1ef16f464aaafd0f7bb159aad5a5b4ec
Jenny <5704@holbertonstudents.com>
2023-02-05 00:47:23 -0800

    Fixed if statement

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -60,6 +60,6 @@ class Rectangle:
         Defines the perimeter of the rectangle
         """
         if self.__width == 0 or self.__height == 0:
-            return = 0
+            return = 0:
         else:
             return 2 * (self.__width + self.__height)
