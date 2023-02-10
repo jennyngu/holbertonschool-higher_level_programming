316d68bcd022044aeb7d6d82fdaa979d2917d890
Jenny <5704@holbertonstudents.com>
2023-02-10 04:19:22 -0800

    Added a method that replaces all attributes of a student instance

---
+++ python-input_output/11-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+A class named Student
+"""
+
+
+class Student:
+    """
+    A class that defines students
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Instantiation of students
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Return: dict representation of Student instances
+        """
+        attr_dict = {}
+        if type(attrs) == list:
+            for att in attrs:
+                if hasattr(self, att):
+                    attr_dict[att] = getattr(self, att)
+                    return attr_dict
+        return(self.__dict__)
+
+    def reload_from_json(self, json):
+        """
+        Replaces all attributes of the Student instance
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
