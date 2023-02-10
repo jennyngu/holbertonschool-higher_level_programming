24a86402488d8a61fc737623947be22a56e1fa33
Jenny <5704@holbertonstudents.com>
2023-02-09 16:30:11 -0800

    Fixed pycodestyle

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -4,6 +4,7 @@ A function that converts a JSON string to an object (Python data structure)
 """
 import json
 
+
 def from_json_string(my_str):
     """
     Return: object representated by a JSON string
