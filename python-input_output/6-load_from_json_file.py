d53f3ba4490feb0673fcdd4556a620d4d6f01c82
Jenny <5704@holbertonstudents.com>
2023-02-09 16:55:00 -0800

    Fixed pycodestyle

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -10,4 +10,4 @@ def load_from_json_file(filename):
     Return: object created from specified JSON file
     """
     with open(filename, encoding="utf-8") as obj_from_json:
-       return(json.load(obj_from_json))
+        return(json.load(obj_from_json))
