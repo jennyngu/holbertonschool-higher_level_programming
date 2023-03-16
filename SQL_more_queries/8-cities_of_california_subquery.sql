a4db34088252b74f61c152f6ecf7e379860c2948
Jenny <5704@holbertonstudents.com>
2023-03-15 18:05:27 -0700

    Switched California with its id

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- a script that lists all the cities of California that can be found in the database hbtn_0d_usa
 SELECT id, name
 FROM cities
-WHERE name = California
+WHERE state_id = 1
 ORDER BY id ASC;
