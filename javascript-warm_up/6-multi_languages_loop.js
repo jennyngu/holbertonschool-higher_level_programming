89e13e12529ccd2e91a5c1c1d84d0aee05bc64ec
Jenny <5704@holbertonstudents.com>
2023-04-10 06:47:35 -0700

    Added let to properly define variable

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (string = 0; string < array.length; string++) {
+for (let string = 0; string < array.length; string++) {
   console.log(array[string]);
 }
