59c48bb895557095f037ea19a9b698c60e169d51
Jenny <5704@holbertonstudents.com>
2023-04-15 21:08:39 -0700

    Created an instance method that prints Xs to form the rectangle

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || w === undefined) {
+      return;
+    }
+    if (h <= 0 || h === undefined) {
+      return;
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write('X');
+      }
+      console.log('');
+    }
+  }
+};
