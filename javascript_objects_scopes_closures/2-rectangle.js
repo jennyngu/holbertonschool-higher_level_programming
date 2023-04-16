f4d94e264a496b1d8122529d92f7fce35f1f4a25
Jenny <5704@holbertonstudents.com>
2023-04-15 21:07:32 -0700

    Added a class Rectangle that defines a rectangle. Checking value of width and height

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || w === undefined) {
+      return;
+    }
+    if (h <= 0 || h === undefined) {
+      return;
+    }
+    this.width = w;
+    this.height = h;
+  }
+};
