b91895024731254abed5527ac47f324db8fc6d57
Jenny <5704@holbertonstudents.com>
2023-04-20 22:52:25 -0700

    Attempt to use filter to be more concise

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,24 +1,18 @@
 #!/usr/bin/node
 
 const request = require('request');
-const searchWA = 'https://swapi-api.hbtn.io/api/people/18/';
+const charId = 'https://swapi-api.hbtn.io/api/people/18/';
 const url = process.argv[2];
 
 request(url, function (error, response, body) {
   if (error) {
-    console.log(error);
+    console.error(error);
     return;
   }
+  const films = JSON.parse(body).results;
+  const count = films.filter((film) =>
+    film.characters.includes(charId)
+  ).length;
 
-  const bodyInfo = JSON.parse(body);
-  const results = bodyInfo.results;
-
-  let count = 0;
-  for (let i = 0; i < results.length; i++) {
-    const characters = results[i].characters;
-    if (characters.includes(searchWA)) {
-      count = count + 1;
-    }
-  }
   console.log(count);
 });
