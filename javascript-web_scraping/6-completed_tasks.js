58716fb08b51e7b7ed832e5efa6dd4b563b19467
Jenny <5704@holbertonstudents.com>
2023-04-23 03:36:57 -0700

    Fix semistandard

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -2,20 +2,22 @@
 
 const request = require('request');
 const url = process.argv[2];
-request(url, (error, response, body) => {
-    if (error) console.log(error);
-    const todos = JSON.parse(body);
-    const taskCount = {};
 
-    for (let i = 0; i < todos.length; i++) {
-	const todo = todos[i];
-	if (todo.completed) {
-            const userId = todo.userId;
-	    if (!taskCount[userId]) {
-		taskCount[userId] = 0;
-	    }
-	    taskCount[userId] = taskCount[userId] + 1;
+request(url, (error, response, body) => {
+  if (error) console.log(error);
+  const todos = JSON.parse(body);
+  const taskCount = {};
+
+  for (let i = 0; i < todos.length; i++) {
+    const todo = todos[i];
+    if (todo.completed) {
+      const userId = todo.userId;
+      if (!taskCount[userId]) {
+        taskCount[userId] = 0;
       }
-    };
-    console.log(taskCount);
+      taskCount[userId] = taskCount[userId] + 1;
+    }
+  }
+
+  console.log(taskCount);
 });
